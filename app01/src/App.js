1d3ce92dff7e611e73e4a678b167ccf3c5f06dbd
Yuri12 <yuri.00000830267@unicap.br>
2022-08-29 16:20:59 -0300

    continuação dos estudos(01,02,03,04 expressões em jsx(imagemdentro do src e dentro do public)

--- app01/src/App.js
+++ app01/src/App.js
@@ -1,16 +1,22 @@
 import React from 'react'
-
+import Logo from './componentes/imgs/facu.jpg'
+//import react from './componentes/imgs/react.jpg'
 export default function App(){
-
+const canal=()=>{
+     return ('CBF Cursos')
+}
+const curso = 'Curso de React'
 return(
-      <>
-      
-      <p> CBb Cursos</p>
-      <br/>
-      <p>Curso de React</p>
-      
-      
-      </>
+      <section>
+      <header>
+      <p> {"CANAL:"+canal()}</p>
+      <p>{curso}</p>
+      </header>
+      <section>
+      <img src={Logo}/>
+      <img src='/img/front.jpg'/>
+      </section>
+      </section>
 
 
 
