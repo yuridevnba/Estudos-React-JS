6a236bd05e388a933e8b8ad2b8b0b06983d9ec09
Yuri12 <yuri.00000830267@unicap.br>
2022-08-30 11:10:44 -0300

    continuação dos estudos(01,02,03,04 ,05,06,7 enviando funções, com componentes props)

--- app01/src/App.js
+++ app01/src/App.js
@@ -1,15 +1,10 @@
 import React from 'react'
 import Header from './componentes/imgs/Header'
 import Corpo from './componentes/imgs/Corpo'
-//import Logo from './componentes/imgs/facu.jpg'
-//import react from './componentes/imgs/react.jpg'
-import Dados from './componentes/imgs/Dados'
 
 export default function App(){
 
-const yt=' youtube.com/cfbcursos'  
-const canal=' CFB Cursos'
-const crs=" React.js"
+
 
 return(
       <>
@@ -17,14 +12,8 @@ return(
        <h1> Universidade Católica, abre curso de React. </h1>
        <Header/>
        <hr></hr>
-       <p>
-       <Dados canal={canal} youtube ={yt} curso={crs}/> 
-       </p>
       <Corpo/>
       
-      
-     
-    
 
       </>
 )
--- app01/src/componentes/imgs/Corpo.js
+++ app01/src/componentes/imgs/Corpo.js
@@ -1,10 +1,31 @@
 import React from 'react'
+import Dados from './Dados'
+
+
 
 export default function Corpo(){
 
+    const canal=()=>{
+        return ' CFB Cursos'
+        
+        }
+        
+        const yt=' youtube.com/cfbcursos'  
+        //const canal=' CFB Cursos'
+        const crs=" React.js"
+        
+        const somar=(v1,v2)=>{
+            return v1+v2
+        }
     return (
+
         <section>
-            
+            <Dados 
+            canal={canal} 
+            youtube ={yt} 
+            curso={crs} 
+            somar={somar}/> 
+
             <h2> Curso de React</h2>
             <p>  Se inscreva em nosso canal</p>
             <p> Ative no sininho e clique na joinha </p>
