21ab10b0cdc1de7283a89180bd3e9aaba45321bc
Yuri12 <yuri.00000830267@unicap.br>
2022-08-31 16:57:05 -0300

    continuação dos estudos(01,02,03,04 ,05,06,07,08 ,09,10, State)

--- app01/src/App.js
+++ app01/src/App.js
@@ -1,20 +1,25 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Header from './componentes/imgs/Header'
 import Corpo from './componentes/imgs/Corpo'
 import './Appp.css'
 import Relogio from './componentes/imgs/relogio'
 export default function App(){
 
+const[num,setNum]=useState(10) //nome do estado, e função que altera o etado.
+let n1=35
 const testeDestaque={
 
       color:'#00f',
-      fontSize:'3em'image.png
+      fontSize:'3em'
 }
 
 return(
       <>
+      <p>{n1}</p>
+      <p>Valor da state num:{num}</p>
+      <button onClick={()=>n1=100}>100</button> 
       <section className='caixa'>
-            <Relogio/>
+            <Relogio />
       <h1  style={{color:'#f00', fontSize:'3em'}}>CFB Cursos</h1>
        <h1 style={testeDestaque}> Universidade Católica, abre curso de React. </h1>
        <Header/>
