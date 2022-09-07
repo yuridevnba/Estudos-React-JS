2a49d6e2aa35c6359608316b0a4a6b204bdf7897
Yuri12 <yuri.00000830267@unicap.br>
2022-09-07 13:13:45 -0300

    continuação dos estudos(01,02,03,04 ,05,06,07,08 ,09,10,11,12 ,13 Listas)

--- app01/src/Lista.js
+++ app01/src/Lista.js
@@ -1,30 +1,24 @@
 import React from 'react'
 
 export default function Lista(){
-    const carros=['HRV','GOLF','FOCUS','CRUZE','ARGO']
-    const listadeCarros = carros.map(
-        (c,i)=>
-        <li key ={i}>{i}-{c}</li>
-
-
-    )
+    
+    const carros=[
+{categoria: "Esporte", preco:"110000.00",modelo:"Gof GTI"},
+{categoria: "Esporte", preco:"120000.00",modelo:"Camaro"},
+{categoria: "SUV", preco:"850000.00",modelo:"HRV"},
+{categoria: "SUV", preco:"830000.00",modelo:"T-Cross"},
+{categoria: "Utilitário", preco:"120000.00",modelo:"Hillux"},
+{categoria: "Utilitário", preco:"90000.00",modelo:"Range"},
+    ]
+const listaCarros = carros .map(
+(c,i)=>
+<li key={i}>{i}- {c.categoria}-{c.modelo}- R${c.preco}</li>
+)
+    
 return(
 <>
 
-
-
-
-<ul>{listadeCarros}</ul>
-
-
-
-
-
-
-
-
-
-
+<ul>{listaCarros}</ul>
 
 </>
 )
