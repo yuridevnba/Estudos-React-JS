8674dc663833287b379b7425080ea65a0d2bec75
Yuri12 <yuri.00000830267@unicap.br>
2022-09-08 10:44:27 -0300

    continuação dos estudos(01,02,03,04 ,05,06,07,08 ,09,10,11,12 ,13 ,14 Formulários)

--- app01/src/formulario.js
+++ app01/src/formulario.js
@@ -1,22 +1,43 @@
 import React,{useState}  from "react";
 
 export default function Formulario(){
-
+    const [carro,setCarro]=useState('HRV')
     const[nome,setNome]=useState('')
 
+
+    const handleChangeNome=(e)=>{
+        setNome(e.target.value)
+}
 return(
 <>
 
 
 
 <label>Digite seu Nome</label>
-
-<input type="text" name="fnome" value={nome} onChange={(e)=>setNome(e.target.value)}/>
-
-<p>Nome Digitado: </p>
+<input type="text" name="fnome" value={nome} onChange={(e)=>handleChangeNome(e)}></input>
+<p>Nome Digitado:{nome} </p>
 
 
-{nome}
+
+
+<label>Selecione um Carro</label>
+<select value={carro} onChange={(e)=>setCarro(e.target.value)}>
+<option value="HRV">HRV</option>
+<option value="GOLF">GOLF</option>
+<option value="Cruze">Cruze</option>
+<option value="Argo">Argo</option>
+</select>
+
+<p>Carro Selecionado: {carro}</p>
+
+
+
+
+
+
+
+
+
 
 
 
